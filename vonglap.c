94e2a9c8487b3a9db586470aa3992ae53d5fac46
xibachaos <57268085+xibachaos@users.noreply.github.com>
2022-07-12 04:02:49 +0700

    for if continue

---
+++ vonglap.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(){
+	int i, n, s;
+	printf("Nhap so bat ky: "); scanf("%d",&n);
+	for (i = 0; i = 1 ; i += 1){
+		if (s = s + i) continue;
+		printf ("%d", s);
+	}
+	
+}	
+
+
+// Bang cuu chuong
+/*int main () {
+	int i, j, n = 10, m = 10;
+	for (i = 1; i <= n; i++){
+		for (j = 1; j <= m; j++)
+				printf ("%d x %2d = %2d\t", i, j, i*j);
+		printf ("\n");
+	}
+	return 0;
+}
+// Ma Tran
+int main () {
+	int i, j, n = 5, m = 10;
+	for (i = 0; i < n; i++){
+		for (j = 0; j < m; j++)
+				printf("*\t");	//printf ("%d", i + j);
+			printf ("\n");
+	}
+	return 0;
+}
+int main(int argc, char *argv[]) {
+	int i;
+	for (i = 0; i < 100; i+=5)	
+	printf ("%d\n", i);
+	return 0;
+	
+}
+int main(int argc, char *argv[]) {
+	for (int i = 0; i < 100; i = i + 5)  	
+	printf ("%d\n", i);
+	return 0;
+	
+}
+int main(int argc, char *argv[]) {
+	int i;
+	for (i = 0; i < 10; i++)  
+	printf ("%d\n", i);
+	printf ("\n");
+	return 0;
+	
+}*/
