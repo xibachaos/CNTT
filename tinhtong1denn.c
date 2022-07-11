d707fca134eab3d244948226a22b8dd2d287d519
xibachaos <57268085+xibachaos@users.noreply.github.com>
2022-07-12 04:04:11 +0700

    for if continue

---
+++ tinhtong1denn.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(){
+	int i, s=0, n;
+	printf("Nhap vao so: ");
+	scanf("%d", &n);
+
+	for (i=0; i<=n; i++)
+		
+		if (s=s+i) 
+		continue;
+		printf("Tong la: %d", s);
+	
+	return 0;	
+}
